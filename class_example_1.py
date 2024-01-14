eed4a5a213dd4a9870a84c4725a8ffecad0d241f
EtoOgueji <etoogueji@gmail.com>
2024-01-14 23:01:08 +0100

    class example 1

---
+++ class_example_1.py
@@ -0,0 +1,15 @@
+class Car():
+    def __init__(self, name, model, year, color):
+        self.name =name
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def drive():
+        return "The car is driving"
+    
+    def rocket_mode():
+        return "launch sequence initiated!!!"
+    
+    def nuke():
+        return "Goodbye World!!!"
