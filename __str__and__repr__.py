730fd3d750ef1a91bde2b9465c7500a3826c764a
EtoOgueji <etoogueji@gmail.com>
2024-01-14 22:59:52 +0100

    str and repr 1

---
+++ __str__and__repr__.py
@@ -0,0 +1,10 @@
+# str: turns an object into a string objecy
+# repr: returns a string representation of an object
+
+l = ["Python", "Java", "C++", "Perl"]
+
+print(l)
+
+print(str(l)) # '["Python", "Java", "C++", "Perl"]'
+
+print(repr(l)) # '["Python", "Java", "C++", "Perl"]'
