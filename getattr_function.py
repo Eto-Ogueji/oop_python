37aad0d83ae60f599b7c8eacf97fc34c1d0ad067
EtoOgueji <etoogueji@gmail.com>
2024-01-14 23:05:05 +0100

    gettattr function

---
+++ getattr_function.py
@@ -0,0 +1,18 @@
+# shit didn't work
+
+class Robot:
+    pass
+
+Robot.brand = "Ogueji"
+
+x = Robot()
+x.name = "ETG-V1"
+
+getattr(x, 'energy', 100)
+
+y = Robot()
+z = Robot
+
+
+print(x,"\n", y,"\n", z)
+print(x.energy)
